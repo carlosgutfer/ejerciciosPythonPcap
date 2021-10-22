1c6ed709f39780236334e39782f7fd5241c13f72
carlosgutfer <carlos.gutfer@hotmail.es>
2021-10-22 10:45:55 +0200

    221021

---
+++ filter.py
@@ -0,0 +1,11 @@
+﻿from random import seed, randint
+
+seed()
+data = [randint(-10,10) for x in range(5)]
+filtered = list(filter(lambda x: x > 0 and x % 2 == 0, data))
+
+print(data)
+print(filtered)
+
+filper al igualque map toma los mismos argumentos, pero ahora puedes hacer un condicionante y solo devuelve
+los que seantrue
---
+++ outer.py
@@ -0,0 +1,12 @@
+﻿def outer(par):
+    loc = par
+
+    def inner():
+        return loc
+    return inner
+
+
+var = 1
+fun = outer(var)
+print(fun())
+ Devuelve el valor que hay dentro de la función inner que está dentro de la función
